6a0ba6dbef88ffe267e98dd750f640db2d551511
Chris-TopherW <wratt_c@actrix.co.nz>
2018-09-16 09:45:45 +1200

    Added actual FMOD banks and scripts

---
+++ Assets/Scripts/TestFMOD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class TestFMOD : MonoBehaviour {
+
+	public string thePath = "event:/MainSong";
+    [Range(1.0f, 4.0f)]
+    public float section = 3.0f;
+    FMOD.Studio.EventInstance musicEvent;
+
+	private void Start() {
+        musicEvent = FMODUnity.RuntimeManager.CreateInstance(thePath);
+        musicEvent.start();
+
+	}
+
+    private void Update()
+    {
+        musicEvent.setParameterValue("Section", section);
+    }
+
+    //void PlaySound(string path)
+    //{
+    //		FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+
+    //}
+}
