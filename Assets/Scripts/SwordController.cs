6cdcf7018d4ae02f758404fce68bd20c5e49cb11
EmperorJack <emperorpurvis@gmail.com>
2018-09-15 14:26:44 +1200

    Very bad sword mechanic

---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour {
+
+    private Quaternion startingRotation;
+    public bool animating = false;
+    public float animationPercent = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        startingRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            animating = true;
+            animationPercent = 0.0f;
+        }
+
+        if (animating) {
+            animationPercent += 0.05f;
+        }
+
+        //float t = animationPercent < 0.5f ? animationPercent * 2.0f : 1.0f - (animationPercent - 0.5f) * 2.0f;
+        float t = animationPercent < 0.5f ? Map(animationPercent, 0.0f, 0.5f, 0.0f, 1.0f) : Map(animationPercent, 0.5f, 1.0f, 1.0f, 0.0f);
+
+        transform.rotation = Quaternion.Lerp(
+            startingRotation,
+            startingRotation * Quaternion.AngleAxis(180, Vector3.up),
+            t
+        );
+
+        if (animating && animationPercent >= 1.0f) {
+            animating = false;
+            animationPercent = 0.0f;
+        }
+    }
+
+    public float Map(float x, float in_min, float in_max, float out_min, float out_max)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+}
