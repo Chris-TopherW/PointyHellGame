9049ace1da5a9d94c7c128b005951095eb63918c
Chris-TopherW <wratt_c@actrix.co.nz>
2018-09-16 17:02:05 +1200

    Add music param setter func

--- Assets/Scripts/TestFMOD.cs
+++ Assets/Scripts/TestFMOD.cs
@@ -22,6 +22,12 @@ public class TestFMOD : MonoBehaviour {
         musicEvent.setParameterValue("Section", section);
     }
 
+    //1 is intro screen, 3 is main loop. 2.0 to 3 can be lerped for music fade in.
+    public void setMusicParam(float param)
+    {
+        section = param;
+    }
+
     //void PlaySound(string path)
     //{
     //		FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
